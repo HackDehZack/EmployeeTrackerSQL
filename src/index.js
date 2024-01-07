2f0b2a522eea40da9fada29e55d59fb2c9cc6dfa
HackDehZack <Zackseriousemail@gmail.com>
2024-01-07 05:08:51 -0500

    Add server setup code

--- src/index.js
+++ src/index.js
@@ -148,6 +148,17 @@ function promptUpdateEmployeeRole() {
 }
 
 
+
+
+//server localhost 
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server started on http://localhost:${3000}`);
+});
+
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
