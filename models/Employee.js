e6e380eaf0c06d4548dc6bb16c60c44252dfab8d
HackDehZack <Zackseriousemail@gmail.com>
2024-01-07 05:23:03 -0500

    Refactor Employee class to use static methods

--- models/Employee.js
+++ models/Employee.js
@@ -1,10 +1,9 @@
-// Necessary modules (use require statements)
 const connection = require('../db/connection');
 const queries = require('../db/queries');
 
 class Employee {
   // Method to retrieve all employees from the database
-  async getAllEmployees() {
+  static async getAllEmployees() {
     try {
       // Implement the SQL query here using the queries module
       const employees = await queries.getAllEmployees();
@@ -16,7 +15,7 @@ class Employee {
   }
 
   // Method to add a new employee to the database
-  async addEmployee(firstName, lastName, roleId, managerId) {
+  static async addEmployee(firstName, lastName, roleId, managerId) {
     try {
       // Implement the SQL query here using the queries module
       const result = await queries.addEmployee(firstName, lastName, roleId, managerId);
@@ -28,7 +27,7 @@ class Employee {
   }
 
   // Method to update an employee's role in the database
-  async updateEmployeeRole(employeeId, roleId) {
+  static async updateEmployeeRole(employeeId, roleId) {
     try {
       // Implement the SQL query here using the queries module
       const result = await queries.updateEmployeeRole(employeeId, roleId);
@@ -40,7 +39,7 @@ class Employee {
   }
 
   // Method to update an employee's manager in the database
-  async updateEmployeeManager(employeeId, managerId) {
+  static async updateEmployeeManager(employeeId, managerId) {
     try {
       // Implement the SQL query here using the queries module
       const result = await queries.updateEmployeeManager(employeeId, managerId);
