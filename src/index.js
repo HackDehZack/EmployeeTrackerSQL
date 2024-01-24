baa38ddb5719cf1194348e4218e2844715e72df0
HackDehZack <Zackseriousemail@gmail.com>
2024-01-24 16:27:23 -0500

    revamp the index bc i evil

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const inquirer = require("inquirer");
 const db = require("../db/connection");
+const { table } = require("console");
 
 const executeQuery = async (query, replacements = []) => {
   try {
@@ -13,31 +14,101 @@ const executeQuery = async (query, replacements = []) => {
   }
 };
 
+const getDepartments = async () => {
+  return await executeQuery("SELECT id, name FROM department");
+};
+
 const getRoles = async () => {
-  return await executeQuery("SELECT id, title FROM role");
+  return await executeQuery(
+    "SELECT role.id, role.title, department.name AS department_name FROM role INNER JOIN department ON role.department_id = department.id"
+  );
 };
 
 const getEmployees = async () => {
-  return await executeQuery("SELECT id, CONCAT(first_name, ' ', last_name) AS name FROM employee");
+  return await executeQuery(
+    "SELECT employee.id, employee.first_name, employee.last_name, role.title AS role_title, department.name AS department_title, role.salary AS role_salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager_name FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee AS manager ON employee.manager_id = manager.id"
+  );
 };
 
-const formatEmployeeData = (employees) => {
-  return employees.map(employee => ({
-    ID: employee.id,
-    First_Name: employee.first_name,
-    Last_Name: employee.last_name,
-    Title: employee.role_title,
-    Department: employee.department_title,
-    Salary: employee.role_salary,
-    Manager: `${employee.manager_first_name} ${employee.manager_last_name}`
-  }));
+const viewAllDepartments = async () => {
+  const departments = await getDepartments();
+  if (departments) {
+    console.log(table(departments));
+  }
+  start();
+};
+
+const viewAllRoles = async () => {
+  const roles = await getRoles();
+  if (roles) {
+    console.log(table(roles));
+  }
+  start();
 };
 
 const viewAllEmployees = async () => {
-  // const query = `...`; 
-  const employees = await executeQuery(query);
+  const employees = await getEmployees();
   if (employees) {
-    console.table(formatEmployeeData(employees));
+    console.log(table(employees));
+  }
+  start();
+};
+
+const addDepartment = async () => {
+  const answers = await inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "Enter the name of the department:"
+    }
+  ]);
+
+  try {
+    await db.query("INSERT INTO department SET ?", {
+      name: answers.name
+    });
+    console.log("Department added!");
+  } catch (error) {
+    console.error(error);
+  }
+  start();
+};
+
+const addRole = async () => {
+  const departments = await getDepartments();
+  if (!departments) return start();
+
+  const answers = await inquirer.prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "Enter the title of the role:"
+    },
+    {
+      type: "input",
+      name: "salary",
+      message: "Enter the salary for the role:"
+    },
+    {
+      type: "list",
+      name: "department_id",
+      message: "Select the department for the role:",
+      choices: departments.map(department => ({
+        name: department.name,
+        value: department.id
+      }))
+    }
+  ]);
+
+  try {
+    await db.query("INSERT INTO role SET ?", {
+      title: answers.title,
+      salary: answers.salary,
+      department_id: answers.department_id
+    });
+    console.log("Role added!");
+  } catch (error) {
+    console.error(error);
   }
   start();
 };
@@ -46,8 +117,36 @@ const addEmployee = async () => {
   const roles = await getRoles();
   const employees = await getEmployees();
   if (!(roles && employees)) return start();
-  
+
   const answers = await inquirer.prompt([
+    {
+      type: "input",
+      name: "first_name",
+      message: "Enter the employee's first name:"
+    },
+    {
+      type: "input",
+      name: "last_name",
+      message: "Enter the employee's last name:"
+    },
+    {
+      type: "list",
+      name: "role_id",
+      message: "Select the employee's role:",
+      choices: roles.map(role => ({
+        name: role.title,
+        value: role.id
+      }))
+    },
+    {
+      type: "list",
+      name: "manager_id",
+      message: "Select the employee's manager:",
+      choices: employees.map(employee => ({
+        name: `${employee.first_name} ${employee.last_name}`,
+        value: employee.id
+      }))
+    }
   ]);
 
   try {
@@ -64,6 +163,40 @@ const addEmployee = async () => {
   start();
 };
 
+const updateEmployeeRole = async () => {
+  const employees = await getEmployees();
+  const roles = await getRoles();
+  if (!(employees && roles)) return start();
+
+  const answers = await inquirer.prompt([
+    {
+      type: "list",
+      name: "employee_id",
+      message: "Select the employee to update:",
+      choices: employees.map(employee => ({
+        name: `${employee.first_name} ${employee.last_name}`,
+        value: employee.id
+      }))
+    },
+    {
+      type: "list",
+      name: "role_id",
+      message: "Select the employee's new role:",
+      choices: roles.map(role => ({
+        name: role.title,
+        value: role.id
+      }))
+    }
+  ]);
+
+  try {
+    await db.query("UPDATE employee SET role_id = ? WHERE id = ?", [answers.role_id, answers.employee_id]);
+    console.log("Employee role updated!");
+  } catch (error) {
+    console.error(error);
+  }
+  start();
+};
 
 const start = async () => {
   console.log("Welcome to the Employee Tracker!");
@@ -73,9 +206,43 @@ const start = async () => {
       name: "action",
       message: "What would you like to do?",
       choices: [
+        "View all departments",
+        "View all roles",
+        "View all employees",
+        "Add a department",
+        "Add a role",
+        "Add an employee",
+        "Update an employee role",
+        "Exit"
       ]
     });
-    await action();
+
+    switch (action) {
+      case "View all departments":
+        viewAllDepartments();
+        break;
+      case "View all roles":
+        viewAllRoles();
+        break;
+      case "View all employees":
+        viewAllEmployees();
+        break;
+      case "Add a department":
+        addDepartment();
+        break;
+      case "Add a role":
+        addRole();
+        break;
+      case "Add an employee":
+        addEmployee();
+        break;
+      case "Update an employee role":
+        updateEmployeeRole();
+        break;
+      case "Exit":
+        console.log("Goodbye!");
+        process.exit(0);
+    }
   } catch (error) {
     console.error(error);
     start();
@@ -87,7 +254,12 @@ start();
 
 // Export functions for testing
 module.exports = {
+  viewAllDepartments,
+  viewAllRoles,
   viewAllEmployees,
+  addDepartment,
+  addRole,
   addEmployee,
+  updateEmployeeRole,
   start
 };
